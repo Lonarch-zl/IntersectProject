c7eb7f953fbdee283c5a72c0e3b954448b37ce5a
Lonarch-zl <747852583@qq.com>
2020-03-10 11:41:17 +0800

    test commit

---
+++ test/UnitTest1.cpp
@@ -0,0 +1,54 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../IntersectProject/main.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			vector<LINE> lines;
+			LINE line1 = makeline(0, 0, 1, 1);
+			lines.push_back(line1);
+			Assert::AreEqual(calculateCrossPointsNum(lines), 0);
+		}
+		TEST_METHOD(TestMethod2)
+		{
+			vector<LINE> lines;
+			LINE line1 = makeline(0, 0, 1, 1);
+			LINE line2 = makeline(1, 0, 0, 1);
+			LINE line3 = makeline(1, 2, 2, 0);
+			lines.push_back(line1);
+			lines.push_back(line2);
+			lines.push_back(line3);
+			Assert::AreEqual(calculateCrossPointsNum(lines), 3);
+		}
+		TEST_METHOD(TestMethod3)
+		{
+			vector<LINE> lines;
+			LINE line1 = makeline(0, 0, 1, 1);
+			LINE line2 = makeline(0, 0, 0, 1);
+			LINE line3 = makeline(0, 0, -1, 0);
+			lines.push_back(line1);
+			lines.push_back(line2);
+			lines.push_back(line3);
+			Assert::AreEqual(calculateCrossPointsNum(lines), 1);
+		}
+		TEST_METHOD(TestMethod4)
+		{
+			vector<LINE> lines;
+			LINE line1 = makeline(0, 0, 1, 1);
+			LINE line2 = makeline(1, 0, 0, 1);
+			LINE line3 = makeline(0, 0, -1, 1);
+			lines.push_back(line1);
+			lines.push_back(line2);
+			lines.push_back(line3);
+			Assert::AreEqual(calculateCrossPointsNum(lines), 2);
+		}
+	};
+}
